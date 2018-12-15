aacd21513aba4aecf433e6a104d6191b292dd959
tamsamani mohamed <tamsmoha@gmail.com>
2018-12-15 12:24:07 +0100

    Create Page Component
    make Page Component as main Component

---
+++ src/Page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Page extends React.Component {
+	render() {
+		return (
+			<div className="page">
+				<header>Header</header>
+				<main>Main Page Content</main>
+				<footer>Footer</footer>
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Page from "./Page";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Page name="main" />
+				<h1>Hello CodeSandbox</h1>
+				<h2>Start editing to see some magic happen!</h2>
+			</div>
+		);
+	}
 }
 
 const rootElement = document.getElementById("root");
